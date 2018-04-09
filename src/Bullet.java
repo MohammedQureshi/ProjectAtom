f8b17344f3753966dd7532020acfc6aa7676bcd6
CyberZealot <37114093+CyberZeal0t@users.noreply.github.com>
2018-04-09 08:47:52 +0100

    Update Bullet.java

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,36 +1,19 @@
 import java.util.ArrayList;
 
-public class Bullet {
+public class Bullet extends Player {
 
 	int bulletY;
 	int bulletX;
 	boolean playerfire = false;
-	boolean monsterfire = false;
 	//private ArrayList bullets;
 	
 	//*Jack*//
-	//Split into two, aim is to create a bullet for both monsters and players using the same method//
 	public Bullet() {
 		if (playerfire == true) {
 			bulletY ++;
+			
 		}
-		if (monsterfire == true) {
-			bulletY --;
-		}
-	}
-	
-	//*Jack*//
-	//Alternate layout, two methods//
-	public void playerFire() {
-		if (playerfire == true) {
-			bulletY ++;
-		}
-	}
-	
-	public void monsterFire() {
-		if (monsterfire == true) {
-			bulletY --;
-		}
+		
 	}
 	
 	//Setters & getters//

6fd3c104683dc6bdd8fc434a3f8cc43fdc2ad736
CyberZealot <37114093+CyberZeal0t@users.noreply.github.com>
2018-04-17 14:15:01 +0100

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -3,14 +3,14 @@ import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Player extends Actor implements KeyListener{
+public class Player extends Actor  implements KeyListener{
 	
-	private int XPos = 0;
+	protected int XPos;
 	private int YPos = 380;
 	//private int Random = 100;
 	private int Height;
 	private int Width;
-	private int currentPos;
+	protected int currentPos;
 	//private int PlayerLives = 3;
 	
 	public Player(SpaceInvaders spaceInvaders) {
@@ -20,7 +20,67 @@ public class Player extends Actor implements KeyListener{
 	public Player() {
 		
 	}
+	
+	public void paint(Graphics2D g) {
+		/* Mohammed / Mark */
+		g.drawImage(Stage.Monster, XPos, YPos,SpriteCache.Image.getWidth() * 5,SpriteCache.Image.getHeight() * 5, null);
+		
+	}
 
+	public Rectangle getBounds() {
+		return null;
+	}
+
+	public void collision(Actor a1) {
+		
+	}
+
+	public void keyPressed(KeyEvent key) {
+		/* Mohammed */
+		if(key.getKeyCode() == KeyEvent.VK_LEFT) {
+			if (XPos == 0) {
+				XPos -=0;
+			}
+			else {
+				XPos -= 10;
+				currentPos = XPos;
+			}
+		}
+		if (key.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if (XPos >= Stage.WIDTH - 61) {
+				XPos +=0;
+			}
+			else {
+				XPos += 10;
+				currentPos = XPos;
+			}
+		}
+		if (key.getKeyCode() == KeyEvent.VK_SPACE) {
+			System.out.println(currentPos);
+			new Bullet();
+
+		}
+	}
+	
+	/* Mohammed */
+	public void keyReleased(KeyEvent key) {
+		XPos = currentPos;
+	}
+	
+	
+
+	public int getCurrentPos() {
+		return currentPos;
+	}
+
+	public void setCurrentPos(int currentPos) {
+		this.currentPos = currentPos;
+	}
+
+	public void keyTyped(KeyEvent key) {
+		
+	}
+	
 	public int getX() {
 		return XPos;
 	}
@@ -54,45 +114,5 @@ public class Player extends Actor implements KeyListener{
 	}
 
 	public void act() {
-	}
-
-	public Rectangle getBounds() {
-		return null;
-	}
-
-	public void collision(Actor a1) {
-		
-	}
-
-	public void paint(Graphics2D g) {
-		/* Mohammed / Mark */
-		g.drawImage(Stage.Monster, XPos, YPos,SpriteCache.Image.getWidth() * 5,SpriteCache.Image.getHeight() * 5, null);
-		
-	}
-
-	public void keyPressed(KeyEvent key) {
-		/* Mohammed */
-		if(key.getKeyCode() == KeyEvent.VK_LEFT) { 
-			XPos -= 10;
-			currentPos = XPos;
-		}
-		if (key.getKeyCode() == KeyEvent.VK_RIGHT) {
-			XPos += 10;
-			currentPos = XPos;
-		}
-		if (key.getKeyCode() == KeyEvent.VK_SPACE) {
-			//FIRE//
-		}
-		
-	}
-	
-	/* Mohammed */
-	public void keyReleased(KeyEvent key) {
-		XPos = currentPos;
-		
-	}
-
-	public void keyTyped(KeyEvent key) {
-		
 	}
 }

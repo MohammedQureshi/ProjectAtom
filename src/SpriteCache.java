ddfda0e2d4f556c4577c03ae8c83901b87c8fd1d
CyberZealot <37114093+CyberZeal0t@users.noreply.github.com>
2018-04-19 12:34:03 +0100

    Update SpriteCache.java

--- src/SpriteCache.java
+++ src/SpriteCache.java
@@ -5,14 +5,28 @@ import javax.imageio.*;
 public class SpriteCache {
 	
 	private String fileName = "monster";
-	//private String fileName1 = "player";
-	//private String fileName2 = "bullet";
+	private String fileName1 = "player";
+	private String fileName2 = "bullet";
 	public static BufferedImage Image;
+	public static BufferedImage Image2;
+	public static BufferedImage Image3;
 	
 	public SpriteCache() {
 		try{
 			Image = ImageIO.read(new File("res/"+fileName+".png"));
 			System.out.println(fileName + " File Loaded");
+		}catch(IOException ioException) {
+			ioException.printStackTrace();
+		}
+		try{
+			Image2 = ImageIO.read(new File("res/"+fileName2+".png"));
+			System.out.println(fileName2 + " File Loaded");
+		}catch(IOException ioException) {
+			ioException.printStackTrace();
+		}
+		try{
+			Image3 = ImageIO.read(new File("res/"+fileName1+".png"));
+			System.out.println(fileName1 + " File Loaded");
 		}catch(IOException ioException) {
 			ioException.printStackTrace();
 		}
